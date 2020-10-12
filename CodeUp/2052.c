9847b2a33867785b83e738baae6fcb2d6afc4598
hg_yellow <63451752+jang010505@users.noreply.github.com>
2020-10-12 21:50:38 +0900

    Create 2052.c

---
+++ CodeUp/2052.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+  int x, i, j;
+	for(i=0;i<5;i++){
+		scanf("%d", &x);
+		int res=1;
+		for(j=2;j*j<=x;j++){
+			if(x%j==0){
+				res+=j;
+				if(x/j!=j) res+=x/j;
+			}
+		}
+		if(res==x) printf("yes\n");
+		else printf("no\n");
+	}
+}
