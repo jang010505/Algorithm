e814029d003ab0d2b9abea04009b47dc05eaa014
hg_yellow <63451752+jang010505@users.noreply.github.com>
2020-10-19 22:09:27 +0900

    Create 8716.py

---
+++ BOJ/8716.py
@@ -0,0 +1,27 @@
+x1, y1, x2, y2=map(int, input().split())
+x3, y3, x4, y4=map(int, input().split())
+if x1<=x3 and x4<=x2:
+    if y1>=y3 and y4>=y2: print(abs((y4-y3)*(x3-x4)))
+    elif y3>=y1 and y2>=y4: print(abs((y2-y1)*(x3-x4)))
+    elif y1>=y3 and y3>=y2: print(abs((x3-x4)*(y3-y2)))
+    elif y3>=y1 and y1>=y4: print(abs((x3-x4)*(y1-y4)))
+    else: print(0)
+elif x3<=x1 and x2<=x4:
+    if y1>=y3 and y4>=y2: print(abs((y4-y3)*(x1-x2)))
+    elif y3>=y1 and y2>=y4: print(abs((y2-y1)*(x1-x2)))
+    elif y1>=y3 and y3>=y2: print(abs((x1-x2)*(y3-y2)))
+    elif y3>=y1 and y1>=y4: print(abs((x1-x2)*(y1-y4)))
+    else: print(0)
+elif x1<=x3 and x3<=x2:
+    if y1>=y3 and y4>=y2: print(abs((y4-y3)*(x3-x2)))
+    elif y3>=y1 and y2>=y4: print(abs((y2-y1)*(x3-x2)))
+    elif y1>=y3 and y3>=y2: print(abs((x3-x2)*(y3-y2)))
+    elif y3>=y1 and y1>=y4: print(abs((x3-x2)*(y1-y4)))
+    else: print(0)
+elif x3<=x1 and x1<=x4:
+    if y1>=y3 and y4>=y2: print(abs((y4-y3)*(x4-x1)))
+    elif y3>=y1 and y2>=y4: print(abs((y2-y1)*(x4-x1)))
+    elif y1>=y3 and y3>=y2: print(abs((x4-x1)*(y3-y2)))
+    elif y3>=y1 and y1>=y4: print(abs((x4-x1)*(y1-y4)))
+    else: print(0)
+else: print(0)
