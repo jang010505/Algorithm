4cc71fed5a8c47a464d52274a27405059194e3c1
hg_yellow <63451752+jang010505@users.noreply.github.com>
2020-10-22 23:11:07 +0900

    Update 16428.py

--- BOJ/16428.py
+++ BOJ/16428.py
@@ -1,7 +1,16 @@
 x, y=map(int, input().split())
 if y<0:
-    print(x//y+1)
-    print(abs(x%y)-1)
+    if x<0:
+        if x%y!=0:
+            print(x//y+1)
+            print(x-(x//y+1)*y)
+        else:
+            print(x//y)
+            print(x%y)
+    else:
+        if x%y!=0: print(x//y+1)
+        else: print(x//y)
+        print(x%abs(y))
 else:
     print(x//y)
     print(x%y)
