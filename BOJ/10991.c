af0e93540985ae824afc3c85ffa4dff0aa334e22
hg_yellow <63451752+jang010505@users.noreply.github.com>
2020-10-30 19:55:20 +0900

    Create 10991.c

---
+++ BOJ/10991.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int n, i, j;
+	scanf("%d", &n);
+	for(i=0;i<n;i++){
+		for(j=0;j<n-i-1;j++) printf(" ");
+		printf("*");
+		for(j=0;j<2*(i-1)+1;j++){
+			if(j%2) printf("*");
+			else printf(" ");
+		}
+		if(i) printf("*");
+		printf("\n");
+	}
+}
