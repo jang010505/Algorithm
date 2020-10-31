4d6cc0ea24f8f358b434722cc3a5797f0f38b751
hg_yellow <63451752+jang010505@users.noreply.github.com>
2020-11-01 02:51:44 +0900

    Create 15891.c

---
+++ BOJ/15891.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int d[0];
+int main(void){
+	int a;
+	scanf("%d", &a);
+	if(a==1) printf("65");
+	if(a==2) printf("17");
+	if(a==3) printf("4");
+	if(a==4) printf("4");
+	if(a==5) printf("64");
+}
