2c73256f9356468fc29fef31b00dcfb88c3ba480
hg_yellow <63451752+jang010505@users.noreply.github.com>
2020-11-18 21:50:11 +0900

    Create 14592.c

---
+++ BOJ/14592.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	int n, d[3][3]={}, max=0;
+	scanf("%d", &n);
+	for(int i=0;i<n;i++){
+		scanf("%d %d %d", &d[i][0], &d[i][1], &d[i][2]);
+		if(d[max][0]<d[i][0]) max=i;
+		if(d[max][0]==d[i][0] && d[max][1]>d[i][1]) max=i;
+		if(d[max][0]==d[i][0] && d[max][1]==d[i][1] && d[max][2]>d[i][2]) max=i;
+	}
+	printf("%d", max+1);
+}
