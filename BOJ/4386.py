d3732d908d9d15ce46aa176a0324e57b43026afe
hg_yellow <63451752+jang010505@users.noreply.github.com>
2021-01-21 19:10:21 +0900

    Create 4386.py

---
+++ BOJ/4386.py
@@ -0,0 +1,33 @@
+def find(num):
+    if parent[num]==num:
+        return num
+    else:
+        parent[num]=find(parent[num])
+        return parent[num]
+def union(num1, num2):
+    a=find(num1)
+    b=find(num2)
+    if a!=b:
+        parent[b]=a
+def parentFind(num1, num2):
+    if find(num1)==find(num2):
+        return 1
+    else:
+        return 0
+n=int(input())
+x_list=list()
+node=list()
+for i in range(n):
+    a, b=map(float, input().split())
+    x_list.append((a, b))
+for i in range(n):
+    for j in range(i+1, n):
+        node.append((i+1, j+1, ((x_list[i][0]-x_list[j][0])**2+(x_list[i][1]-x_list[j][1])**2)**(1/2)))
+answer=0
+parent=[i for i in range(n+1)]
+node.sort(key=lambda x : x[-1])
+for i in range(len(node)):
+    if parentFind(node[i][0], node[i][1])==0:
+        union(node[i][0], node[i][1])
+        answer+=node[i][2]
+print(answer)
