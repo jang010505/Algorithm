f50fc9471a9e8ee1d7295a24174d2dfe19fbf9fa
hg_yellow <63451752+jang010505@users.noreply.github.com>
2021-02-01 17:51:25 +0900

    Update 계수정렬.c

--- 알고리즘/정렬/계수정렬.c
+++ 알고리즘/정렬/계수정렬.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 
+void countingSort(int d[], int count[], int n){
+	for(int i=0;i<n;i++)
+		count[d[i]]++;
+	return;
+}
+void print(int count[], int max){
+	for(int i=1;i<=max;i++)
+		for(int j=0;j<count[i];j++)
+			printf("%d ", i);
+	return;
+}
 int main(){
-	int n, m, d[1000], cnt[1000]={};
-	scanf("%d %d", &n, &m);
-	for(int i=0;i<n;i++) scanf("%d", &d[i]);
-	for(int i=0;i<n;i++) cnt[d[i]]++;
-	for(int i=1;i<=m;i++){
-		for(int j=0;j<cnt[i];j++) printf("%d ", i);
+	int n, m, d[100001], count[100001]={}, max=0;
+	scanf("%d", &n); 
+	for(int i=0;i<n;i++){
+		scanf("%d", &d[i]);
+		if(max<d[i])
+			max=d[i];
 	}
+	countingSort(d, count, n);
+	print(count, max);
+	return 0;
 }
