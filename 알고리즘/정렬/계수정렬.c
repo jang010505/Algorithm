5dd380ecba2152a298f2cf4601161bdda80acb09
hg_yellow <63451752+jang010505@users.noreply.github.com>
2021-02-01 17:58:33 +0900

    Create 계수정렬.c

--- 알고리즘/정렬/계수정렬.c
+++ 알고리즘/정렬/계수정렬.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
 
-void countingSort(int d[], int count[], int n){
-	for(int i=0;i<n;i++)
-		count[d[i]]++;
+void bubbleSort(int d[], int n){
+	for(int i=0;i<n-1;i++){
+		for(int j=0;j<n-i-1;j++){
+			if(d[j]>d[j+1]){
+				int temp=d[j];
+				d[j]=d[j+1];
+				d[j+1]=temp;
+			}
+		}
+	}
 	return;
 }
-void print(int count[], int max){
-	for(int i=1;i<=max;i++)
-		for(int j=0;j<count[i];j++)
-			printf("%d ", i);
-	return;
+void print(int d[], int n){
+	for(int i=0;i<n;i++)
+		printf("%d ", d[i]);
 }
 int main(){
-	int n, m, d[100001], count[100001]={}, max=0;
-	scanf("%d", &n); 
-	for(int i=0;i<n;i++){
+	int n, d[100001];
+	scanf("%d", &n);
+	for(int i=0;i<n;i++)
 		scanf("%d", &d[i]);
-		if(max<d[i])
-			max=d[i];
-	}
-	countingSort(d, count, n);
-	print(count, max);
+	bubbleSort(d, n);
+	print(d, n);
 	return 0;
 }
