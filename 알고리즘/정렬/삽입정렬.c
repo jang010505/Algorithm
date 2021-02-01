772808d811e9491433f1e11d275c3385bcd208fb
hg_yellow <63451752+jang010505@users.noreply.github.com>
2021-02-01 18:10:45 +0900

    Update 삽입정렬.c

--- 알고리즘/정렬/삽입정렬.c
+++ 알고리즘/정렬/삽입정렬.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
 
-int main(){
-	int n, d[1000], temp;
-	scanf("%d", &n);
-	for(int i=0;i<n;i++) scanf("%d", &d[i]);
+void insertionSort(int d[], int n){
 	for(int i=0;i<n-1;i++){
 		int j=i;
 		while(d[j]>d[j+1]){
-			temp=d[j];
+			int temp=d[j];
 			d[j]=d[j+1];
 			d[j+1]=temp;
 			j--;
 		}
 	}
-	for(int i=0;i<n;i++) printf("%d ", d[i]);
+	return;
+}
+void print(int d[], int n){
+	for(int i=0;i<n;i++)
+		printf("%d ", d[i]);
+	return;
+}
+int main(){
+	int n, d[100001];
+	scanf("%d", &n);
+	for(int i=0;i<n;i++)
+		scanf("%d", &d[i]);
+	insertionSort(d, n);
+	print(d, n);
+	return 0;
 }
