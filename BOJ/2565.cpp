681f6ccdb2dadcb0d64a6deac6bc5c4d8f1b5e46
hg_yellow <63451752+jang010505@users.noreply.github.com>
2021-02-05 16:49:57 +0900

    Update 2565.cpp

--- BOJ/2565.cpp
+++ BOJ/2565.cpp
@@ -1,1 +1,25 @@
+#include <cstdio>
+#include <algorithm>
+#include <vector>
 
+using namespace std;
+
+int main(){
+	int n, dp[100]={}, answer=0;
+	scanf("%d", &n);
+	vector <pair<int, int> > v(n);
+	for(int i=0;i<n;i++)
+		scanf("%d %d", &v[i].first, &v[i].second);
+	sort(v.begin(), v.end());
+	for(int i=1;i<n;i++){
+		int tmp_a=v[i].first;
+		int tmp_b=v[i].second;
+		for(int j=0;j<i;j++){
+			if(tmp_a>v[j].first && tmp_b>v[j].second)
+				dp[i]=max(dp[i], dp[j]+1); 
+		}
+		answer=max(answer, dp[i]);
+	}
+	printf("%d", n-answer-1);
+    return 0;
+}
