fcab10e92896f0b26be3867dd15189de98a9fe26
hg_yellow <jang010505@gmail.com>
2021-02-23 01:30:50 +0900

    .

---
+++ BOJ/20921.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    int n, k, d[4242] = {}, visit[4243] = {};
+    scanf("%d %d", &n, &k);
+    int start = 1, end = n;
+    for (int i = 0; i < n; i++)
+    {
+        if (k >= n - 1 - i)
+        {
+            while (visit[end])
+                end--;
+            visit[end] = 1;
+            d[i] = end--;
+            k -= n - 1 - i;
+        }
+        else
+        {
+            while (visit[start])
+                start++;
+            d[i] = start++;
+        }
+    }
+    for (int i = 0; i < n; i++)
+        printf("%d ", d[i]);
+}
